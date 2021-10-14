fa711923e2b9c46631b78888c058750d4b74b5cb
Meead Alotaibi <meeadalotaibi@Meeads-MacBook-Pro.local>
2021-10-14 14:33:49 +0300

    added task 1 and 2

---
+++ dataTypes.js
@@ -0,0 +1,20 @@
+let age  = 15 ;
+let name = "Renad";
+let arr = [35];
+let obj = {
+    num : 45 
+};
+//display veriaples eith typeOf
+console.log (age + " " + typeof age)  ;
+console.log (name + " " + typeof name);
+console.log (arr + " " + typeof arr);
+console.log (obj.num + " " + typeof obj.num);
+
+// Array with string and number value 
+let arrStudents =['Meead' , 25 , "Sahar" , 24];
+console.log(typeof arrStudents);
+
+// created null variable and an undefined variable
+let nullVariable = null ;
+let undefinedVariable = undefined;
+
---
+++ myVariables.js
@@ -0,0 +1,15 @@
+ let language = "JavaScript";
+ let score = 10 ;
+ console.log( language + score )
+ let length , width , area;
+ length = 10;
+ width = 5 ;
+  area = length * width
+
+console.log("The area of the rectangle is " + area + " where "+ length +" and " + width +" is the area you calculated.");
+ let num1 ,num2 , result ;
+ num1 = 7;
+ num2 = 2 ;
+ result = num1 % num2 ;
+ console.log(result)
+console.log( num1 + " / " + num2 + " = " + num1 / num2  + " remainder " + result)
